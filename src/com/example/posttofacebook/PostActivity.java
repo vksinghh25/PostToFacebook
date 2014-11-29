018ccd621e7b871624c47f74569d249beda68366
vksinghh25 <vipan.k.singh@gmail.com>
2014-11-29 00:08:26 -0500

    PostToFacebook
    A simple Android app integrated with the Facebook SDK that lets you
    share stuff on Facebook using the FB native app installed on the same
    device.

---
+++ src/com/example/posttofacebook/PostActivity.java
@@ -0,0 +1,128 @@
+package com.example.posttofacebook;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
+
+public class PostActivity extends Activity {
+
+	private UiLifecycleHelper uiHelper;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_post);
+
+		uiHelper = new UiLifecycleHelper(this, 
+			new Session.StatusCallback() {
+			@Override
+			public void call(Session session, SessionState state, 
+				Exception exception) {
+				// this is much like our Congress
+				// does nothing for now
+			}
+		});
+		uiHelper.onCreate(savedInstanceState);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction().add(R.id.container, 
+					new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.post, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+			@Override
+			public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+				Log.e("Activity", String.format("Error: %s", error.toString()));
+			}
+
+			@Override
+			public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+				Log.i("Activity", "Success!");
+			}
+		});
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		uiHelper.onResume();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_post, container, false);
+			return rootView;
+		}
+	}
+
+	public void shareOnFB(View view) {
+		String linkToBeShared = "https://play.google.com/store/apps/developer?id=vksinghh";
+		String caption = "VkSingh on Google Play!!";
+
+		FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(this).setLink(linkToBeShared)
+				.setCaption(caption).build();
+		uiHelper.trackPendingDialogCall(shareDialog.present());
+	}
+}
